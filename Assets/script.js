2bfa2d9c63f5ce87e61ec80c32ed1031c9e85992
Pat Downer <patricia.downer@gmail.com>
2020-07-24 02:28:57 -0700

    identifies correct answer when clicked

--- Assets/script.js
+++ Assets/script.js
@@ -1,56 +1,43 @@
-// When the 'start' button is clicked...
-document.getElementById('start').addEventListener('click', (event) => {
+// Score counter variable
+let score = 0
 
-  // stop the default behavior of buttons
-  event.preventDefault()
+// List of questions to pull from later
+let qList = [
 
-  // will need to address start timer here somewhere
+  //Each item in the array is a new question (Q) with answers (A, B, C, and D) and the correct answer.
+  {
+    Q: 'What color is the sky?',
+    A: 'Red',
+    B: 'Yellow',
+    C: 'Blue',
+    D: 'Green',
+    Correct: 'C'
+  },
+  {
+    Q: 'What character is not a Mario character?',
+    A: 'Mario',
+    B: 'Tails',
+    C: 'Toad',
+    D: 'Yoshi',
+    Correct: 'B'
+  },
+  {
+    Q: 'The early bird gets the ____.',
+    A: 'coffee',
+    B: 'prize',
+    C: 'work',
+    D: 'worm',
+    Correct: 'D'
+  }
 
-  // Score counter variable
-  let score = 0
+]
 
-  // List of questions to pull from later
-  let qList = [
+// Need to figure out how to make it randomly work its way through the questions from the above qList WITHOUT repeating any. Does the Math.random thingy have a way to avoid repetition?? Or do I create another array that puts the numbers 0-19 (if 20 questions total) in the array in random order, then work my way through that array in order to pull question numbers from??
+//For now, let's just get a random item number of the array
+let qNum = parseInt(Math.floor(Math.random() * qList.length))
 
-    //Each item in the array is a new question (Q) with answers (A, B, C, and D) and the correct answer.
-    {
-      Q: 'What color is the sky?',
-      A: 'Red',
-      B: 'Yellow',
-      C: 'Blue',
-      D: 'Green',
-      Correct: 'C'
-    },
-    {
-      Q: 'What character is not a Mario character?',
-      A: 'Mario',
-      B: 'Tails',
-      C: 'Toad',
-      D: 'Yoshi',
-      Correct: 'B'
-    },
-    {
-      Q: 'The early bird gets the ____.',
-      A: 'coffee',
-      B: 'prize',
-      C: 'work',
-      D: 'worm',
-      Correct: 'D'
-    }
-
-  ]
-
-  // Just testing the function of the qList above.
-  console.log(qList[Math.floor(Math.random() * qList.length)].Q)
-  console.log(qList[Math.floor(Math.random() * qList.length)].A)
-  console.log(qList[Math.floor(Math.random() * qList.length)].B)
-  console.log(qList[Math.floor(Math.random() * qList.length)].C)
-  console.log(qList[Math.floor(Math.random() * qList.length)].D)
-  console.log(qList[Math.floor(Math.random() * qList.length)].Correct)
-
-  // Need to figure out how to make it randomly work its way through the questions from the above qList WITHOUT repeating any. Does the Math.random thingy have a way to avoid repetition?? Or do I create another array that puts the numbers 0-19 (if 20 questions total) in the array in random order, then work my way through that array in order to pull question numbers from??
-  //For now, let's just get a random item number of the array
-  let qNum = parseInt(Math.floor(Math.random() * qList.length))
+// Function to pick a question
+const pickQ = () => {
 
 
   //replace content of question div in HTML
@@ -68,4 +55,59 @@ document.getElementById('start').addEventListener('click', (event) => {
   <button id="btnC" class="btn btn-primary btn-lg">C. `+ qList[qNum].C + `</button>
   <button id="btnD" class="btn btn-primary btn-lg">D. `+ qList[qNum].D + `</button>
     `
+
+}
+
+
+// When the 'start' button is clicked...
+document.getElementById('start').addEventListener('click', (event) => {
+
+  // stop the default behavior of buttons
+  event.preventDefault()
+
+  // will need to address start timer here somewhere
+
+
+  pickQ()
+
+  let ans = qList[qNum].Correct
+
+  console.log(ans)
+
+  //Whan answer A is clicked ...
+  document.getElementById('btnA').addEventListener('click', (event) => {
+    if (qList[qNum].Correct === 'A') {
+      console.log('Right!')
+    } else {
+      console.log('Wrong!')
+    }
+  })
+
+  //Whan answer B is clicked ...
+  document.getElementById('btnB').addEventListener('click', (event) => {
+    if (qList[qNum].Correct === 'B') {
+      console.log('Right!')
+    } else {
+      console.log('Wrong!')
+    }
+  })
+
+  //Whan answer C is clicked ...
+  document.getElementById('btnC').addEventListener('click', (event) => {
+    if (qList[qNum].Correct === 'C') {
+      console.log('Right!')
+    } else {
+      console.log('Wrong!')
+    }
+  })
+
+  //Whan answer D is clicked ...
+  document.getElementById('btnD').addEventListener('click', (event) => {
+    if (qList[qNum].Correct === 'D') {
+      console.log('Right!')
+    } else {
+      console.log('Wrong!')
+    }
+  })
 })
+
