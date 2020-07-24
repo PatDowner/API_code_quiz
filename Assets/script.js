e8025f1947e3af37aea8d75d291a69396166fa43
Pat Downer <patricia.downer@gmail.com>
2020-07-24 01:40:59 -0700

    pick random question (qNum) and swap in that q & a

--- Assets/script.js
+++ Assets/script.js
@@ -30,7 +30,7 @@ document.getElementById('start').addEventListener('click', (event) => {
       Correct: 'B'
     },
     {
-      Q: 'The early bird gets the ____',
+      Q: 'The early bird gets the ____.',
       A: 'coffee',
       B: 'prize',
       C: 'work',
@@ -41,40 +41,31 @@ document.getElementById('start').addEventListener('click', (event) => {
   ]
 
   // Just testing the function of the qList above.
-  console.log(qList[1].Q)
-  console.log(qList[1].A)
-  console.log(qList[1].B)
-  console.log(qList[1].C)
-  console.log(qList[1].D)
-  console.log(qList[1].Correct)
+  console.log(qList[Math.floor(Math.random() * qList.length)].Q)
+  console.log(qList[Math.floor(Math.random() * qList.length)].A)
+  console.log(qList[Math.floor(Math.random() * qList.length)].B)
+  console.log(qList[Math.floor(Math.random() * qList.length)].C)
+  console.log(qList[Math.floor(Math.random() * qList.length)].D)
+  console.log(qList[Math.floor(Math.random() * qList.length)].Correct)
 
   // Need to figure out how to make it randomly work its way through the questions from the above qList WITHOUT repeating any. Does the Math.random thingy have a way to avoid repetition?? Or do I create another array that puts the numbers 0-19 (if 20 questions total) in the array in random order, then work my way through that array in order to pull question numbers from??
+  //For now, let's just get a random item number of the array
+  let qNum = parseInt(Math.floor(Math.random() * qList.length))
+
 
   //replace content of question div in HTML
   document.getElementById('question').innerHTML = `
   <h3 class="display-4">
-    `+ qList[1].Q + `
+    `+ qList[qNum].Q + `
   </h3>
   `
 
+
   // Replace content of answers p in HTML
   document.getElementById('answers').innerHTML = `
-  <button class="btn btn-primary btn-lg">A. `+ qList[1].A + `</button>
-  <button class="btn btn-primary btn-lg">B. `+ qList[1].B + `</button>
-  <button class="btn btn-primary btn-lg">C. `+ qList[1].C + `</button>
-  <button class="btn btn-primary btn-lg">D. `+ qList[1].D + `</button>
+  <button id="btnA" class="btn btn-primary btn-lg">A. `+ qList[qNum].A + `</button>
+  <button id="btnB" class="btn btn-primary btn-lg">B. `+ qList[qNum].B + `</button>
+  <button id="btnC" class="btn btn-primary btn-lg">C. `+ qList[qNum].C + `</button>
+  <button id="btnD" class="btn btn-primary btn-lg">D. `+ qList[qNum].D + `</button>
     `
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
